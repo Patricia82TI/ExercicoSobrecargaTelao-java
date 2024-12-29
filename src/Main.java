a680e8c77748f16c2548624f6ab328c8705045b4
Patricia82TI <patricia.melloti@gmail.com>
2024-12-29 06:26:20 -0300

    Exercicio Sobrecarga Telao

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+
+//Faça um programa com uma classe telao com dois metodos de exibir.
+//Um recebe um número de um jogador e um nome e imprime
+//O outro recebe apenas o número do jogador e imprime.
+public class Main {
+    public static void main(String[] args) {
+    Telao telao = new Telao();
+    telao.exibir(20);
+    telao.exibir(25,"João");
+
+    }
+}
